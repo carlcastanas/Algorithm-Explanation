a0fba6de5dd0a3cac832da57954c064f0ca1becf
Carl Andrew Castañas <89068725+carlcastanas@users.noreply.github.com>
2023-04-16 00:39:03 +0800

    Create Linear Structures.py

---
+++ Linear Structures.py
@@ -0,0 +1,33 @@
+# Create a list of integers
+my_list = [1, 2, 3, 4, 5]
+
+# Access the first element of the list
+first_element = my_list[0]
+
+# Access the last element of the list
+last_element = my_list[-1]
+
+# Append a new element to the end of the list
+my_list.append(6)
+
+# Insert a new element at a specific index
+my_list.insert(3, 7)
+
+# Remove an element from the list
+my_list.remove(2)
+
+# Get the length of the list
+list_length = len(my_list)
+
+# Check if an element is in the list
+element_is_in_list = 5 in my_list
+
+# Print the resulting list and some of its properties
+print(my_list)
+print(f"The first element is {first_element}")
+print(f"The last element is {last_element}")
+print(f"The list has {list_length} elements")
+print(f"Is 5 in the list? {element_is_in_list}")
+"""
+This code creates a list of integers, accesses its first and last elements, appends a new element to the end, inserts a new element at a specific index, removes an element from the list, gets the length of the list, and checks if a specific element is in the list. Finally, it prints the resulting list and some of its properties.
+Note that there are many other operations that can be performed on lists and other linear structures, such as sorting, reversing, slicing, and more.
